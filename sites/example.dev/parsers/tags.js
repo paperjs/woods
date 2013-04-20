9c34015aa67ecd606b9f66ccc6f3088a31f5920a
Jonathan Puckey <me@jonathanpuckey.com>
2013-04-20 15:05:14 +0200

    Change the way we handle parsers.
    No longer base the name off the filename, opting instead to have parser files export an object literal of which the key/values define parsers.

--- sites/example.dev/parsers/tags.js
+++ sites/example.dev/parsers/tags.js
@@ -1,22 +1,24 @@
-module.exports = function(string, page) {
-	// Split on commas surrounded by 0 or more spaces:
-	var names = string.split(/\s*,\s*/g);
-	if (!names.length)
-		return;
-	var site = page.site,
-		siteTags = site.tags,
-		pageTags = page.tags;
-	if (!siteTags) siteTags = site.tags = [];
-	if (!pageTags) pageTags = page.tags = [];
-	names.forEach(function(name) {
-		// Note: site.tags is being treated
-		// as an object literal and array:
-		var tag = siteTags[name];
-		if (!tag) {
-			tag = siteTags[name] = [];
-			siteTags.push(tag);
-		}
-		tag.push(page);
-		pageTags.push(tag);
-	});
+module.exports = {
+	tags: function(string, page) {
+		// Split on commas surrounded by 0 or more spaces:
+		var names = string.split(/\s*,\s*/g);
+		if (!names.length)
+			return;
+		var site = page.site,
+			siteTags = site.tags,
+			pageTags = page.tags;
+		if (!siteTags) siteTags = site.tags = [];
+		if (!pageTags) pageTags = page.tags = [];
+		names.forEach(function(name) {
+			// Note: site.tags is being treated
+			// as an object literal and array:
+			var tag = siteTags[name];
+			if (!tag) {
+				tag = siteTags[name] = [];
+				siteTags.push(tag);
+			}
+			tag.push(page);
+			pageTags.push(tag);
+		});
+	}
 };
